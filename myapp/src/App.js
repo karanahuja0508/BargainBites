db38c649af874722156a6c8b69f12877ba24e9c5
karanahuja0508 <karanahuja0508@gmail.com>
2023-04-13 02:19:31 +0530

    header complete

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 import { Header } from "./components";
+import { Route, Routes } from "react-router-dom";
 
 const App= ()=>{
-    return <div className= "w-screen h-auto flex flex-col">
+    return <div className= "w-screen h-auto flex flex-col bg-primary">
 
         <Header/>
+        <main className="mt-24 p-8 w-full">
+           <Routes>
+                <Route path="/*" element={<MainContainer/>}/>
+                <Route path="/*" element={<CreateContainer/>}/>
+            
+            </Routes>      
+            
+        </main>
         </div>;
 };
 
--- myapp/src/components/Header.jsx
+++ myapp/src/components/Header.jsx
@@ -1,25 +1,36 @@
 import React from 'react'
 import Logo from './img/logobargainbites.jpg'
+import Avatar from './img/avatar.png'
+import {MdShoppingBasket} from 'react-icons/md'
+
 
 const Header = () => {
   return (
     <Header className="fixed z-50 w-screen  p-6 px-16">
         {/* for desktop */}
-        <div className="hidden md:flex w-full h-full p-4">
+        <div className="hidden md:flex w-full h-full items-center justify-between">
           {/* for logo */}
           <div className="flex items-center gap-1">
             <img src={Logo} className="w-13 h-20 object-cover" alt='logo'></img>
-            <p className="text-headingColor text-xl font-bold">BARGAIN BITES</p>
+            <p className="text-headingColor text-xl font-bold me-8">BARGAIN BITES</p>
           </div>
 
-          <ul className="flex items-center gap-8">
-            <li>HOME</li>
-            <li>MENU</li>
-            <li>ABOUT US</li>
-            <li>SERVICES</li>
+          <div className="flex items-center gap-8">         
+            <ul className="flex items-center gap-8">
+            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">HOME</li>
+            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">MENU</li>
+            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">ABOUT US</li>
+            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">SERVICES</li>
           </ul>
 
-
+          <div className="relative flex items-center justify-center">
+          <MdShoppingBasket className="text-textColor text-2xl cursor-pointer"/>
+          <div className=" absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center">
+            <p className="text-xs text-white font-semibold">2</p>
+          </div>
+          </div>
+          <img src={Avatar} className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl" alt="userprofile" />
+          </div>
         </div>
 
         {/* for mobile */}
