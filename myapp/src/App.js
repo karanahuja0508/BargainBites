44a1c2d4c7cdb1d8462453d1b5d3bb9aa0a33f86
karanahuja0508 <karanahuja0508@gmail.com>
2023-04-13 22:59:02 +0530

    onestepdone

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,20 +1,22 @@
 import React from "react";
 import { Header } from "./components";
+import { MainContainer , CreateContainer } from "./components";
 import { Route, Routes } from "react-router-dom";
 
 const App= ()=>{
-    return <div className= "w-screen h-auto flex flex-col bg-primary">
-
+    return (
+    <div className= "w-screen h-auto flex flex-col">
         <Header/>
         <main className="mt-24 p-8 w-full">
            <Routes>
                 <Route path="/*" element={<MainContainer/>}/>
-                <Route path="/*" element={<CreateContainer/>}/>
+                <Route path="/createitem" element={<CreateContainer/>}/>
             
             </Routes>      
             
         </main>
-        </div>;
+        </div>
+    );
 };
 
 export default App;
--- myapp/src/components/Header.jsx
+++ myapp/src/components/Header.jsx
@@ -1,43 +1,51 @@
-import React from 'react'
-import Logo from './img/logobargainbites.jpg'
-import Avatar from './img/avatar.png'
-import {MdShoppingBasket} from 'react-icons/md'
-
-
-const Header = () => {
+import React from "react";
+import Logo from "../img/logobargainbites.jpg";
+import { MdShoppingCart } from "react-icons/md";
+import Avatar from '../img/avatar.png';
+export const Header = () => {
   return (
-    <Header className="fixed z-50 w-screen  p-6 px-16">
-        {/* for desktop */}
-        <div className="hidden md:flex w-full h-full items-center justify-between">
-          {/* for logo */}
-          <div className="flex items-center gap-1">
-            <img src={Logo} className="w-13 h-20 object-cover" alt='logo'></img>
-            <p className="text-headingColor text-xl font-bold me-8">BARGAIN BITES</p>
-          </div>
-
-          <div className="flex items-center gap-8">         
-            <ul className="flex items-center gap-8">
-            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">HOME</li>
-            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">MENU</li>
-            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">ABOUT US</li>
-            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">SERVICES</li>
+    <header className="fixed z-50 w-screen p-6 px-10">
+      {/* fordesktop */}
+      <div className="hidden md:flex w-full h-full items-center justify-between">
+        <div className="flex items-center gap-1">
+          <img
+            src={Logo}
+            className="w-13 h-20 object-cover"
+            alt="Bargain Bites"
+          />
+          <p className="text-headingcolor text-md font-bold me-8">
+            Bargain-Bites
+          </p>
+        </div>
+        <div className="flex items-center gap-8">
+          <ul className="flex items-center gap-8">
+            <li className="text-base text text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+              Home
+            </li>
+            <li className="text-base text text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+              Menu
+            </li>
+            <li className="text-base text text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+              About us
+            </li>
+            <li className="text-base text text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+              Service
+            </li>
           </ul>
 
-          <div className="relative flex items-center justify-center">
-          <MdShoppingBasket className="text-textColor text-2xl cursor-pointer"/>
-          <div className=" absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center">
-            <p className="text-xs text-white font-semibold">2</p>
+          <div className="flex items-center justify-center">
+            <MdShoppingCart className="text-textColor text-2xl cursor-pointer"></MdShoppingCart>
+            <div className=" absolute top-10 right-24 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center">
+              <p className="text-xs text-white font-semibold">2</p>
+            </div>
           </div>
-          </div>
-          <img src={Avatar} className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl" alt="userprofile" />
-          </div>
+          <img src={Avatar} className = "w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer" alt="userprofile" />
         </div>
-
-        {/* for mobile */}
-        <div className="flex md:hidden w-full h-full bg-blue-500 p-4"></div>
-      
-    </Header>
-  )
-}
+      </div>
+      {/* for logo */}
+      <div className="flex md:hidden w-full h-full"></div>
+    </header>
+  );
+};
 
 export default Header;
