8fe3dae1714d9db97779aa81062df7e0267dab0b
karanahuja0508 <karanahuja0508@gmail.com>
2023-04-14 00:50:21 +0530

    authentication done

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,9 +2,11 @@ import React from "react";
 import { Header } from "./components";
 import { MainContainer , CreateContainer } from "./components";
 import { Route, Routes } from "react-router-dom";
+import {AnimatePresence} from "framer-motion";
 
 const App= ()=>{
     return (
+    <AnimatePresence> 
     <div className= "w-screen h-auto flex flex-col">
         <Header/>
         <main className="mt-24 p-8 w-full">
@@ -15,7 +17,8 @@ const App= ()=>{
             </Routes>      
             
         </main>
-        </div>
+    </div>
+    </AnimatePresence>
     );
 };
 
--- myapp/src/components/Header.jsx
+++ myapp/src/components/Header.jsx
@@ -1,13 +1,28 @@
 import React from "react";
 import Logo from "../img/logobargainbites.jpg";
-import { MdShoppingCart } from "react-icons/md";
+
+import { MdLogin, MdShoppingCart } from "react-icons/md";
+import{motion} from "framer-motion";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import {app} from "../firebase.config";
+
 import Avatar from '../img/avatar.png';
+import{Link} from "react-router-dom"
 export const Header = () => {
+
+  const firebaseAuth = getAuth(app);
+  const provider= new GoogleAuthProvider();
+  const login = async () =>{
+    const response =await signInWithPopup(firebaseAuth,provider);
+    console.log(response);
+
+  };
+
   return (
     <header className="fixed z-50 w-screen p-6 px-10">
       {/* fordesktop */}
       <div className="hidden md:flex w-full h-full items-center justify-between">
-        <div className="flex items-center gap-1">
+        <Link to={'/'} className="flex items-center gap-1">
           <img
             src={Logo}
             className="w-13 h-20 object-cover"
@@ -16,7 +31,7 @@ export const Header = () => {
           <p className="text-headingcolor text-md font-bold me-8">
             Bargain-Bites
           </p>
-        </div>
+        </Link>
         <div className="flex items-center gap-8">
           <ul className="flex items-center gap-8">
             <li className="text-base text text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
@@ -39,7 +54,12 @@ export const Header = () => {
               <p className="text-xs text-white font-semibold">2</p>
             </div>
           </div>
-          <img src={Avatar} className = "w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer" alt="userprofile" />
+          <div className="realative">
+          <motion.img whileTap={{scale:0.6 }}
+          src={Avatar} className = "w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer" alt="userprofile"
+          onClick={login}
+          />
+          </div>
         </div>
       </div>
       {/* for logo */}
