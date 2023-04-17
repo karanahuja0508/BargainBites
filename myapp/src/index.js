c36662eb82a2a312843d5634c481e6d6c68c73c1
karanahuja0508 <karanahuja0508@gmail.com>
2023-04-18 02:48:19 +0530

    reducer started

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -4,12 +4,18 @@ import './index.css'
 
 import  {BrowserRouter as Router } from "react-router-dom";
 
-import App from './App'
+import App from './App';
+
+import { StateProvider } from "./context/StateProvider";
+import { initialState } from "./context/initialState";
+import reducer from "./context/reducer";
 
 ReactDOM.render(
    <Router>
-      <App/>
-   </Router>
+      <StateProvider initialState={initialState} reducer={reducer}>
+        <App/>
+      </StateProvider>
+   </Router> 
    ,
    document.getElementById("root")
    );
